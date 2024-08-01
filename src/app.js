ec44e85e892e239bb6a7c9184d6a13ccdeb5c9a8
Yonela Carbajal <yonela.carbajal@tecsup.edu.pe>
2024-08-01 03:42:46 -0500

    Primer Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,52 @@
-/* eslint-disable */
+// Importaciones necesarias
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function generateExcuse() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let excuse =
+    who[Math.floor(Math.random() * who.length)] +
+    " " +
+    action[Math.floor(Math.random() * action.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)];
+
+  return excuse;
+}
+
+function updateExcuse() {
+  console.log("Actualizando excusa...");
+  const excuseElement = document.getElementById("excuse");
+  if (excuseElement) {
+    excuseElement.innerHTML = generateExcuse();
+  } else {
+    console.log("No se encontró el elemento con id 'excuse'");
+  }
+}
+
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
+  updateExcuse();
+
+  const generateExcuseButton = document.getElementById("generateExcuseButton");
+  if (generateExcuseButton) {
+    generateExcuseButton.onclick = updateExcuse;
+  } else {
+    console.log("No se encontró el botón con id 'generateExcuseButton'");
+  }
 };
